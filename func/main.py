b88c9f8e54f541d9b6c1ca34ec4d4d017109fb7d
Vinicius Reis <vro@lionx.com.br>
2022-03-18 18:10:51 -0300

    update libs

--- func/main.py
+++ func/main.py
@@ -11,8 +11,12 @@ def get_ticker_visual_identity():
         url_path = service.create_ticker_url_path(params)
         requests_obj = service.get_requests_object_from_url_path(url_path)
         response = service.get_response_from_url_path(requests_obj)
-        return Response(dumps(response), mimetype="application/json", status=200)
+        return Response(
+            dumps(response), mimetype='application/json', status=200
+        )
     except Exception as error:
-        message = { "message": "Fission: get_ticker_visual_identity"}
+        message = {'message': 'Fission: get_ticker_visual_identity'}
         Gladsheim.error(error, message)
-        return Response(dumps(message), mimetype="application/json", status=403)
+        return Response(
+            dumps(message), mimetype='application/json', status=403
+        )
--- tests/src/test_service.py
+++ tests/src/test_service.py
@@ -27,70 +27,62 @@ class TickerDataBuilder:
 
 
 def test_when_region_br_and_symbol_valid_then_create_url():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("PETRasd").set_region("br").create_dict_params()
+    params = TickerDataBuilder().set_symbol("PETRasd").set_region("br").create_dict_params()
     url_path = create_ticker_url_path(params)
     assert (
-        url_path
-        == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/PETR.png"
+            url_path
+            == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/PETR.png"
     )
 
 
 def test_when_region_br_and_symbol_is_invalid_str_then_create_invalid_url():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("asdPETR").set_region("br").create_dict_params()
+    params = TickerDataBuilder().set_symbol("asdPETR").set_region("br").create_dict_params()
     url_path = create_ticker_url_path(params)
     assert (
-        url_path
-        == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/asdP.png"
+            url_path
+            == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/asdP.png"
     )
 
 
 def test_when_region_and_symbol_not_str_then_return_exception():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol(123).set_region(1).create_dict_params()
+    params = TickerDataBuilder().set_symbol(123).set_region(1).create_dict_params()
     with pytest.raises(ValidationError):
         create_ticker_url_path(params)
 
 
 def test_when_region_not_str_then_return_exception():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("PETR").set_region(1).create_dict_params()
+    params = TickerDataBuilder().set_symbol("PETR").set_region(1).create_dict_params()
     with pytest.raises(ValidationError):
         create_ticker_url_path(params)
 
 
 def test_when_region_not_enum_choices_then_return_exception():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("PETR").set_region("par").create_dict_params()
+    params = TickerDataBuilder().set_symbol("PETR").set_region("par").create_dict_params()
     with pytest.raises(ValidationError):
         create_ticker_url_path(params)
 
 
 def test_when_symbol_not_str_then_return_exception():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol(123).set_region("br").create_dict_params()
+    params = TickerDataBuilder().set_symbol(123).set_region("br").create_dict_params()
     with pytest.raises(ValidationError):
         create_ticker_url_path(params)
 
 
 def test_when_region_us_and_symbol_valid_then_create_url():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("AAPL").set_region("us").create_dict_params()
+    params = TickerDataBuilder().set_symbol("AAPL").set_region("us").create_dict_params()
     url_path = create_ticker_url_path(params)
     assert (
-        url_path
-        == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/us/AAPL.png"
+            url_path
+            == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/us/AAPL.png"
     )
 
 
 def test_when_region_us_and_symbol_is_invalid_str_then_create_invalid_url():
-    ticker = TickerDataBuilder()
-    params = ticker.set_symbol("AAPL123").set_region("us").create_dict_params()
+    params = TickerDataBuilder().set_symbol("AAPL123").set_region("us").create_dict_params()
     url_path = create_ticker_url_path(params)
     assert (
-        url_path
-        == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/us/AAPL123.png"
+            url_path
+            == "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/us/AAPL123.png"
     )
 
 
@@ -114,19 +106,23 @@ class RequestsObj:
         return self
 
 
-@patch('func.src.service.requests.get')
-def test_when_requests_valid_url_then_return_requests_object(mock_get_requests_object):
+@patch("func.src.service.requests.get")
+def test_when_requests_valid_url_then_return_requests_object(mock_requests_get):
     url = "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/PETR.png"
-    mock_get_requests_object.return_value = RequestsObj().set_url(url).set_status_code(200)
+    mock_requests_get.return_value = (
+        RequestsObj().set_url(url).set_status_code(200)
+    )
     requests_response = get_requests_object_from_url_path(url)
     assert requests_response.status_code == 200
     assert requests_response.url == url
 
 
-@patch('func.src.service.requests.get')
+@patch("func.src.service.requests.get")
 def test_when_requests_invalid_url_then_return_requests_object(mock_get_requests_object):
     url = "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/123123PETR.png"
-    mock_get_requests_object.return_value = RequestsObj().set_url(url).set_status_code(403)
+    mock_get_requests_object.return_value = (
+        RequestsObj().set_url(url).set_status_code(403)
+    )
     requests_response = get_requests_object_from_url_path(url)
     assert requests_response.status_code == 403
     assert requests_response.url == url
@@ -136,20 +132,20 @@ def test_when_200_status_code_then_response_true():
     url = "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/PETR.png"
     params = RequestsObj().set_status_code(200).set_url(url)
     response = get_response_from_url_path(params)
-    assert response['status'] is True
-    assert response['logo_uri'] == url
+    assert response["status"] is True
+    assert response["logo_uri"] == url
 
 
 def test_when_403_status_code_then_response_true():
     url = "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/123123PETR.png"
     params = RequestsObj().set_status_code(403).set_url(url)
     response = get_response_from_url_path(params)
-    assert response['status'] is False
-    assert response['logo_uri'] == ''
+    assert response["status"] is False
+    assert response["logo_uri"] == ""
 
 
 def test_when_not_expected_status_code_then_response_raises():
     url = "https://sigame-companies-logo.s3.sa-east-1.amazonaws.com/br/123123PETR.png"
     params = RequestsObj().set_status_code(500).set_url(url)
-    with pytest.raises(Exception, match='Internal server error'):
+    with pytest.raises(Exception, match="Internal server error"):
         get_response_from_url_path(params)
